d374c14cb5ce56fe044008dc351fa525eb5f937f
mhdsy <mhdsy@localhost.localdomain>
2018-02-04 21:58:45 +0200

    Add Search section Add Auto Complete ComboBox Class

--- src/main/java/com/agile/ims/controller/ProductController.java
+++ src/main/java/com/agile/ims/controller/ProductController.java
@@ -6,9 +6,11 @@
 package com.agile.ims.controller;
 
 import com.agile.ims.IMS;
+import com.agile.ims.helper.AutoCompleteComboBoxListener;
 import com.agile.ims.helper.Helper;
 import com.agile.ims.helper.Routes;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDecorator;
 import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
@@ -67,6 +69,12 @@ public class ProductController implements Initializable {
     private HBox searchBoxParent;
     @FXML
     private HBox searchBox;
+    @FXML
+    private JFXComboBox<?> categoryCB;
+    @FXML
+    private JFXComboBox<?> brandCB;
+    @FXML
+    private JFXComboBox<?> supplierCB;
 
     public TableColumn<?, ?> getIdCol() {
         return idCol;
@@ -91,6 +99,29 @@ public class ProductController implements Initializable {
     public void setTableView(TableView<?> tableView) {
         this.tableView = tableView;
     }
+    
+    //Dummy DATA Just For Test.....
+    private static final String[] LISTA = { "Abacate", "Abacaxi", "Ameixa", "Amora", "Araticum", "Atemoia", "Avocado",
+			"Banana prata", "Caju", "Cana descascada", "Caqui", "Caqui Fuyu", "Carambola", "Cereja", "Coco verde",
+			"Figo", "Figo da Índia", "Framboesa", "Goiaba", "Graviola", "Jabuticaba", "Jambo", "Jambo rosa", "Jambolão",
+			"Kino (Kiwano)", "Kiwi", "Laranja Bahia", "Laranja para suco", "Laranja seleta", "Laranja serra d’água",
+			"Laranjinha kinkan", "Lichia", "Lima da pérsia", "Limão galego", "Limão Taiti", "Maçã argentina",
+			"Maçã Fuji", "Maçã gala", "Maçã verde", "Mamão formosa", "Mamão Havaí", "Manga espada", "Manga Haden",
+			"Manga Palmer", "Manga Tommy", "Manga Ubá", "Mangostim", "Maracujá doce", "Maracujá para suco", "Melancia",
+			"Melancia sem semente", "Melão", "Melão Net", "Melão Orange", "Melão pele de sapo", "Melão redinha",
+			"Mexerica carioca", "Mexerica Murcote", "Mexerica Ponkan", "Mirtilo", "Morango", "Nectarina",
+			"Nêspera ou ameixa amarela", "Noni", "Pera asiática", "Pera portuguesa", "Pêssego", "Physalis", "Pinha",
+			"Pitaia", "Romã", "Tamarilo", "Tamarindo", "Uva red globe", "Uva rosada", "Uva Rubi", "Uva sem semente",
+			"Abobora moranga", "Abobrinha italiana", "Abobrinha menina", "Alho", "Alho descascado",
+			"Batata baroa ou cenoura amarela", "Batata bolinha", "Batata doce", "Batata inglesa", "Batata yacon",
+			"Berinjela", "Beterraba", "Cebola bolinha", "Cebola comum", "Cebola roxa", "Cenoura", "Cenoura baby",
+			"Couve flor", "Ervilha", "Fava", "Gengibre", "Inhame", "Jiló", "Massa de alho", "Maxixe", "Milho",
+			"Pimenta biquinho fresca", "Pimenta de bode fresca", "Pimentão amarelo", "Pimentão verde",
+			"Pimentão vermelho", "Quiabo", "Repolho", "Repolho roxo", "Tomate cereja", "Tomate salada",
+			"Tomate sem acidez", "Tomate uva", "Vagem", "Agrião", "Alcachofra", "Alface", "Alface americana",
+			"Almeirão", "Brócolis", "Broto de alfafa", "Broto de bambu", "Broto de feijão", "Cebolinha", "Coentro",
+			"Couve", "Espinafre", "Hortelã", "Mostarda", "Rúcula", "Salsa", "Ovos brancos", "Ovos de codorna",
+			"Ovos vermelhos" };
 
     /**
      * Initializes the controller class.
@@ -100,6 +131,10 @@ public class ProductController implements Initializable {
         //Start with hiden SratchBox
         searchBoxParent.setVisible(false);
         searchBoxParent.getChildren().clear();
+        
+        autoCompleteComboBox(categoryCB);
+        autoCompleteComboBox(brandCB);
+        autoCompleteComboBox(supplierCB);
 
     }
 
@@ -127,6 +162,12 @@ public class ProductController implements Initializable {
 
     @FXML
     private void updateProduct(ActionEvent event) {
+    }
+    
+    private void autoCompleteComboBox(JFXComboBox comboBox) {
+        comboBox.getItems().addAll(LISTA);
+        new AutoCompleteComboBoxListener(comboBox);
+        
     }
 
 }
